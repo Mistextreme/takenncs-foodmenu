73460c0097fdd54b85ec8bce2965cbd361e0ab92
Mist the Goat <62227356+Mistextreme@users.noreply.github.com>
2026-03-04 23:44:07 +0000

    Update food and drink labels in config.lua

--- config.lua
+++ config.lua
@@ -6,17 +6,17 @@ Config.ImageSettings = {
 }
 
 Config.Items = {
-    -- Joogid
-    { label = "Banaani Smuuti", item = "banana_smoothie", price = 90, category = "drinks" },
-    { label = "Jääkokteil", item = "ice_cocktail", price = 90, category = "drinks" },
-    { label = "Jääkohv", item = "ice_coffee", price = 90, category = "drinks" },
-    { label = "Jäätee", item = "icetea", price = 90, category = "drinks" },
-    { label = "Jäälatte", item = "icedlatte", price = 90, category = "drinks" },
+    -- Drinks
+    { label = "Banana Smoothie", item = "banana_smoothie", price = 90, category = "drinks" },
+    { label = "Ice Cocktail", item = "ice_cocktail", price = 90, category = "drinks" },
+    { label = "Ice Coffee", item = "ice_coffee", price = 90, category = "drinks" },
+    { label = "Iced Tea", item = "icetea", price = 90, category = "drinks" },
+    { label = "Iced Latte", item = "icedlatte", price = 90, category = "drinks" },
 
-    -- Söögid
+    -- Food
     { label = "WigWrap", item = "chicken_wrap", price = 120, category = "food" },
-    { label = "Friikad", item = "fried_french_fries", price = 120, category = "food" },
-    { label = "Kahekordne Burger", item = "double_shot", price = 120, category = "food" },
+    { label = "Fries", item = "fried_french_fries", price = 120, category = "food" },
+    { label = "Double Burger", item = "double_shot", price = 120, category = "food" },
     { label = "Hotdog", item = "hotdog", price = 129, category = "food" },
 }
 
@@ -30,7 +30,7 @@ Config.TargetZones = {
             {
                 name = 'open_food_menu',
                 icon = 'fas fa-hamburger',
-                label = 'Vaata menüüd',
+                label = 'View Menu',
                 onSelect = function()
                     TriggerEvent('food_menu:openMenu')
                 end
